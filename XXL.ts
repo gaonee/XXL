b587e6d391295ae1026afd610ab8c09761c4f6fa
gaonee <18551750323@163.com>
2019-05-31 16:52:37 +0800

    Create XXL.ts

---
+++ XXL.ts
@@ -0,0 +1,87 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2019-present.
+//  All rights reserved.
+//  Author: Ma Jian
+//  Email:  841374169@qq.com
+//  Game entry.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+class XXL extends egret.DisplayObjectContainer {
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event: egret.Event) {
+
+        egret.lifecycle.addLifecycleListener((context) => {
+            // custom lifecycle plugin
+
+            context.onUpdate = () => {
+
+            }
+        })
+
+        egret.lifecycle.onPause = () => {
+            egret.ticker.pause();
+        }
+
+        egret.lifecycle.onResume = () => {
+            egret.ticker.resume();
+        }
+
+        this.runGame().catch(e => {
+            console.log(e);
+        })
+
+
+
+    }
+
+    private async runGame() {
+        await this.loadResource()
+        this.createGameScene();
+    }
+
+    private async loadResource() {
+        try {
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
+            await RES.loadConfig("resource/default.res.json", "resource/");
+            await RES.loadGroup("preload", 0, loadingView);
+            this.stage.removeChild(loadingView);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    private createGameScene() {
+        let width = this.stage.stageWidth;
+        let height = this.stage.stageHeight;
+
+        // let upView = new ViewContainer(0, 0, width, (height-width)/2);
+        // this.addChild(upView);
+
+        let mapView = new MapContainer(0, (height-width)/2, width, width);
+        this.addChild(mapView);
+        new Control(mapView);
+    }
+
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name: string) {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+}
