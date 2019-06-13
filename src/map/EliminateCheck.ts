95669455174af1e9274804fd07fa507d45201a73
gaonee <18551750323@163.com>
2019-06-13 19:43:10 +0800

    support special effect

--- src/map/EliminateCheck.ts
+++ src/map/EliminateCheck.ts
@@ -10,20 +10,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
-enum ELIMINATE_TYPE {
-    ROW_LINE_THREE,
-    ROW_LINE_FOUR,
-    ROW_LINE_FIVE,
-    COL_LINE_THREE,
-    COL_LINE_FOUR,
-    COL_LINE_FIVE,
-    NON_LINE
-}
-
 class EliminateCheck {
-
-    constructor() {}
-
     /*
      * 计算本次交换是否会达到消除条件，用于滑动后的首次消除。
      * @param map BlockMap
@@ -33,11 +20,19 @@ class EliminateCheck {
      * @param distCol 目标格子所在列
      * @param dir 手指滑动方向
     **/
-    public touchCheck(map: BlockMap, touchRow: number, touchCol: number, distRow: number, distCol: number, dir: number): Point[] {
-        let touchRet: Point[] = this.singleScan(map, distRow, distCol, map.get(touchRow, touchCol), dir);
-        let distRet: Point[] = this.singleScan(map, touchRow, touchCol, map.get(distRow, distCol), -dir);
+    public touchCheck(map: BlockMap, touchRow: number, touchCol: number, distRow: number, distCol: number, dir: number): EliminateInfo[] {
+        let eliminateList: EliminateInfo[] = new Array();
+        let touchRet: EliminateInfo = this.singleScan(map, distRow, distCol, map.get(touchRow, touchCol), dir);
+        let distRet: EliminateInfo = this.singleScan(map, touchRow, touchCol, map.get(distRow, distCol), -dir);
 
-        return touchRet.concat(distRet);
+        if (touchRet != null) {
+            eliminateList.push(touchRet);
+        }
+        if (distRet != null) {
+            eliminateList.push(distRet)
+        }
+
+        return eliminateList;
     }
 
     /*
@@ -45,13 +40,174 @@ class EliminateCheck {
      * 算法原理：通过两次扫描，找到所有符合条件的格子。算法复杂度为O(2n)。详细原理如下：
      * 1.先逐行扫，找到连续三个或者三个以上相同格子，计入行消除列表；
      * 2.逐列扫描，找到连续三个或者三个以上相同格子，计入列消除列表；
-     * 3.比较两个消除列表，找到重复的格子。（暂未实现）
+     * 3.比较两个消除列表，找到重复的格子；
+     * 4.有交叉表示非线性消除，交叉点为keyPoint；
+     * 5.无交叉为线性消除。
     **/
-    public whollyCheck(map: BlockMap): Point[] {
-        let rowPoints: Point[] = this.scanRow(map);
-        let colPoints: Point[] = this.scanCol(map);
+    public whollyCheck(map: BlockMap): EliminateInfo[] {
+        let eliminateList: EliminateInfo[] = new Array();
+        let rowPoints: Point[][] = this.scanRow(map);
+        let colPoints: Point[][] = this.scanCol(map);
 
-        return rowPoints.concat(colPoints);
+        if (rowPoints.length > 0) {
+            if (colPoints.length > 0) {
+                for (let i = 0; i < rowPoints.length; i++) {
+                    for (let j = 0; j < colPoints.length; j++) {
+                        let keyPoint = this.isCross(rowPoints[i], colPoints[j]);
+                        if (keyPoint != null) {
+                            eliminateList.push({
+                                type: ELIMINATE_TYPE.NON_LINE,
+                                points: rowPoints[i].concat(colPoints[j]),
+                                keyPoint: keyPoint
+                            });
+                            // 产生交叉，该列不再参与后面的比较
+                            colPoints.splice(j, 1);
+                            break;
+                        } else {
+                            if (j == colPoints.length-1) {
+                                // 没有交叉，直接返回
+                                eliminateList.push({
+                                    type: this.getEliminateType("row", rowPoints[i].length),
+                                    points: rowPoints[i],
+                                    keyPoint: this.getKeyPoint(map, "row", rowPoints[i])
+                                });
+                            }
+                        }
+                    }
+                }
+                // 因为只有所有的比较完成，才知道哪些列未交叉，此时需要消除剩余的列
+                this.addLineEliminatePoints(map, "col", colPoints, eliminateList);
+            } else {
+                // 只有行消除
+                this.addLineEliminatePoints(map, "row", rowPoints, eliminateList);
+            }
+        } else {
+            if (colPoints.length > 0) {
+                // 只有列消除
+                this.addLineEliminatePoints(map, "col", colPoints, eliminateList);
+            }
+        }
+
+        return eliminateList;
+    }
+
+    private addLineEliminatePoints(map: BlockMap, axis: string, points: Point[][], eliminateList: EliminateInfo[]) {
+        for (let i = 0; i < points.length; i++) {
+            eliminateList.push({
+                type: this.getEliminateType(axis, points[i].length),
+                points: points[i],
+                keyPoint: this.getKeyPoint(map, axis, points[i])
+            });
+        }
+    }
+
+    /*
+     * 计算本次消除的类型
+     * @param rowArr 消除的行，可为空；不为空时需按行号从小到大排列。
+     * @param colArr 消除的列，可为空；不为空时需按列号从小到大排列。
+     * @param keyPoint 关键点，参考 EliminateInfo 中的说明。
+     * @return EliminateInfo
+    **/
+    private getEliminateInfo(rowArr: Point[], colArr: Point[], keyPoint: Point): EliminateInfo {
+        let eliminateType: ELIMINATE_TYPE = null;
+        let eliminatePoints: Point[] = null;
+
+        if (Array.isArray(rowArr) && rowArr.length >= 3) {
+            if (Array.isArray(colArr) && colArr.length >= 3) {
+                eliminateType = ELIMINATE_TYPE.NON_LINE;
+                eliminatePoints = rowArr.concat(colArr);
+            } else {
+                // 横向线性消除
+                eliminateType = this.getEliminateType("row", rowArr.length);
+                eliminatePoints = rowArr;
+            }
+        } else {
+            // 纵向线性消除
+            if (Array.isArray(colArr) && colArr.length >= 3) {
+                eliminateType = this.getEliminateType("col", colArr.length);
+                eliminatePoints = colArr;
+            }
+        }
+        Log.debug("eliminateType: " + eliminateType);
+        return eliminateType == null ? null : {
+            type: eliminateType,
+            points: eliminatePoints,
+            keyPoint: keyPoint
+        };
+    }
+
+    /*
+     * 计算消除类型
+     * @param rowArr 消除的行，可为空；不为空时需按行号从小到大排列。
+     * @param colArr 消除的列，可为空；不为空时需按列号从小到大排列。
+     * @return ELIMINATE_TYPE
+    **/
+    private getEliminateType(axis: string, eliminateNum: number): ELIMINATE_TYPE {
+        let eliminateType: ELIMINATE_TYPE = null;
+
+        if (axis == "row") {
+            if (eliminateNum == 3) {
+                eliminateType = ELIMINATE_TYPE.ROW_LINE_THREE;
+            } else if (eliminateNum == 4) {
+                eliminateType = ELIMINATE_TYPE.ROW_LINE_FOUR;
+            } else {
+                eliminateType = ELIMINATE_TYPE.ROW_LINE_FIVE;
+            }
+        } else {
+            if (eliminateNum == 3) {
+                eliminateType = ELIMINATE_TYPE.COL_LINE_THREE;
+            } else if (eliminateNum == 4) {
+                eliminateType = ELIMINATE_TYPE.COL_LINE_FOUR;
+            } else {
+                eliminateType = ELIMINATE_TYPE.COL_LINE_FIVE;
+            }
+        }
+
+        return eliminateType;
+    }
+
+    /*
+     * 线性消除的keyPoint
+    **/
+    private getKeyPoint(map: BlockMap, axis: string, points: Point[]) {
+        if (points.length <= 3) {
+            return points[0];
+        }
+        if (axis == "row") {
+            for (let i = 0; i < points.length; i++) {
+                if (map.hasDrop(points[i].row, points[i].col)) {
+                    return points[i];
+                }
+            }
+        } else {
+            for (let i = points.length-1; i >= 0; i--) {
+                if (map.hasDrop(points[i].row, points[i].col)) {
+                    return points[i];
+                }
+            }
+        }
+        return points[0];
+    }
+
+    /*
+     * 判断横向和纵向消除是否交叉。若交叉，返回交叉点；否则，返回null。
+     * @param rowPoints 横向扫描的一组相同点
+     * @param colPoints 纵向扫描的一组相同点
+     * @return 交叉点
+    **/
+    private isCross(rowPoints: Point[], colPoints: Point[]): Point {
+        let rowPos = rowPoints[0].row;
+        let colPos = colPoints[0].col;
+
+        if ((colPos >= rowPoints[0].col && colPos <= rowPoints[rowPoints.length-1].col) ||
+            (rowPos >= colPoints[0].row && rowPos <= colPoints[colPoints.length-1].row)) {
+            return {
+                row: rowPos,
+                col: colPos
+            };
+        }
+
+        return null;
     }
 
     /*
@@ -63,7 +219,7 @@ class EliminateCheck {
      * @param dir 交换方向
      * 注意：交换方向的格子不参与计算，因为肯定无法消除。可不传，不影响正常判断。
     **/
-    private singleScan(map: BlockMap, row: number, col: number, block: BlockBase, dir?: number): Point[] {
+    private singleScan(map: BlockMap, row: number, col: number, block: BlockBase, dir?: number): EliminateInfo {
         let rowAmount: number = map.getRowAmount();
         let colAmount: number = map.getColAmount();
         let colArr: Point[] = new Array();
@@ -110,23 +266,14 @@ class EliminateCheck {
                 }
             }
         }
-        if (colArr.length >= 3) {
-            ret = ret.concat(colArr);
-            Log.debug("删除第 "+colArr[0].col+" 列, 起始行："+colArr[0].row+"，数量：" + colArr.length);
-        }
-        if (rowArr.length >= 3) {
-            ret = ret.concat(rowArr);
-            Log.debug("删除第 "+rowArr[0].row+" 行，起始列："+rowArr[0].col+"，数量：" + rowArr.length);
-        }
-        this.getEliminateInfo(rowArr, colArr, {row: row, col: col});
-        return ret;
+        return this.getEliminateInfo(rowArr, colArr, {row: row, col: col});
     }
 
     /*
      * 逐列扫描，找到所有纵向连续三个或者三个以上相同格子
     **/
-    private scanCol(map: BlockMap): Point[] {
-        let ret: Point[] = new Array();
+    private scanCol(map: BlockMap): Point[][] {
+        let ret: Point[][] = new Array();
         let rowAmount: number = map.getRowAmount();
         let colAmount: number = map.getColAmount();
 
@@ -160,8 +307,8 @@ class EliminateCheck {
     /*
      * 逐行扫描，找到所有横向连续三个或者三个以上相同格子
     **/
-    private scanRow(map: BlockMap): Point[] {
-        let ret: Point[] = new Array();
+    private scanRow(map: BlockMap): Point[][] {
+        let ret: Point[][] = new Array();
         let rowAmount: number = map.getRowAmount();
         let colAmount: number = map.getColAmount();
 
@@ -200,14 +347,16 @@ class EliminateCheck {
      * @param count 本次扫描的相同格子数量
      * @param ret 结果
     **/
-    private settleRow(row: number, col: number, count: number, ret) {
+    private settleRow(row: number, col: number, count: number, ret: Point[][]) {
         if (count >= 3) {
-            for (let i = 0; i < count; i++) {
-                ret.push({
+            let points = new Array();
+            for (let i = count-1; i >= 0; i--) {
+                points.push({
                     row: row,
                     col: col - i
                 });
             }
+            ret.push(points);
         }
     }
 
@@ -218,70 +367,16 @@ class EliminateCheck {
      * @param count 本次扫描的相同格子数量
      * @param ret 结果
     **/
-    private settleCol(row: number, col: number, count: number, ret) {
+    private settleCol(row: number, col: number, count: number, ret: Point[][]) {
         if (count >= 3) {
-            for (let i = 0; i < count; i++) {
-                ret.push({
+            let points = new Array();
+            for (let i = count-1; i >= 0; i--) {
+                points.push({
                     row: row - i,
                     col: col
                 });
             }
+            ret.push(points);
         }
-    }
-
-    /*
-     * 计算本次消除的类型
-     * @param rowArr 消除的行，可为空；不为空时需按行号从小到大排列。
-     * @param colArr 消除的列，可为空；不为空时需按列号从小到大排列。
-     * @param keyPoint 关键点，参考 EliminateInfo 中的说明。
-     * @return EliminateInfo
-    **/
-    private getEliminateInfo(rowArr: Point[], colArr: Point[], keyPoint: Point): EliminateInfo {
-        let eliminateType: ELIMINATE_TYPE = null;
-        let eliminatePoints: Point[] = null;
-
-        if (Array.isArray(rowArr) && rowArr.length >= 3) {
-            if (Array.isArray(colArr) && colArr.length >= 3) {
-                // L型，keyPoint处在rowArr和colArr的两端
-                // T型，keyPoint分别处在rowArr和colArr的两端和中间
-                // if ((keyPoint.col != rowArr[0].col && keyPoint.col != rowArr[rowArr.length-1].col) ||
-                //     (keyPoint.row != colArr[0].row && keyPoint.row != colArr[colArr.length-1].row)) {
-                //     eliminateType = ELIMINATE_TYPE.T_TYPE;
-                // } else {
-                //     eliminateType = ELIMINATE_TYPE.L_TYPE;
-                // }
-                eliminateType = ELIMINATE_TYPE.NON_LINE;
-            } else {
-                // 横向线性消除
-                let len = rowArr.length;
-                if (len == 3) {
-                    eliminateType = ELIMINATE_TYPE.ROW_LINE_THREE;
-                } else if (len == 4) {
-                    eliminateType = ELIMINATE_TYPE.ROW_LINE_FOUR;
-                } else {
-                    eliminateType = ELIMINATE_TYPE.ROW_LINE_FIVE;
-                }
-                eliminatePoints = rowArr;
-            }
-        } else {
-            // 纵向线性消除
-            if (Array.isArray(colArr) && colArr.length >= 3) {
-                let len = colArr.length;
-                if (len == 3) {
-                    eliminateType = ELIMINATE_TYPE.COL_LINE_THREE;
-                } else if (len == 4) {
-                    eliminateType = ELIMINATE_TYPE.COL_LINE_FOUR;
-                } else {
-                    eliminateType = ELIMINATE_TYPE.COL_LINE_FIVE;
-                }
-                eliminatePoints = colArr;
-            }
-        }
-        Log.debug("eliminateType: " + eliminateType);
-        return {
-            type: eliminateType,
-            points: eliminatePoints,
-            keyPoint: keyPoint
-        };
     }
 }
