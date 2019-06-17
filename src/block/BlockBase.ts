3671a0b9c51100f662c4cacffb4b5dbf6279df86
gaonee <18551750323@163.com>
2019-06-17 20:05:39 +0800

    617

--- src/block/BlockBase.ts
+++ src/block/BlockBase.ts
@@ -9,25 +9,99 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 class BlockBase {
-    public block: egret.DisplayObject;
+    public block: egret.Bitmap | egret.MovieClip;
     public info: BlockInfo;
-    public imgRes: string = "bear_png";
     public type: number = BLOCK_TYPE.GREEN;
     public readonly borderRatio: number = 0.1;
     
     public constructor(info: BlockInfo) {
         this.info = info;
-        this.draw();
+        this.initDisplay();
         this.setSize();
     }
 
-    // public functions
-
-    public addTo(container: egret.DisplayObjectContainer) {
-        container.addChild(this.block);
+    private initDisplay() {
+        switch (this.info.effectType) {
+            case EFFECT_TYPE.ROW_LINE:
+            case EFFECT_TYPE.COL_LINE:
+            case EFFECT_TYPE.BOMB:
+            case EFFECT_TYPE.MAGIC_BIRD: {
+                this.block = this.drawSpecialEffect();
+                break;
+            }
+            default: {
+                this.block = this.drawNormal();
+                break;
+            }
+        }
     }
 
-    public change(dir:number, callback?:Function, waitTime?:number) {
+
+
+    /**
+     * 绘制普通状态
+     */
+    public drawNormal(): egret.Bitmap {
+        return Util.createBitmapByName("bear_png");
+    }
+
+    /**
+     * 绘制特效
+     * @return MovieClip对象
+     */
+    public drawSpecialEffect(): egret.MovieClip {
+        return Util.createMCByName("mcbear");
+    }
+
+    /**
+     * 消除成功后，上方格子下落时调用
+     * @param count 下落的格子数
+     * @param callback 下落完成后执行
+     */
+    public drop(count: number, callback?: Function) {
+        let tw = egret.Tween.get(this.block);
+        tw.wait(100).to({
+            y: this.block.y + this.info.height*count
+        }, 200, egret.Ease.quartIn).call(() => {
+            this.info.row += count;
+            egret.Tween.removeTweens(tw);
+            if (callback) {
+                callback();
+            }
+        });
+    }
+
+    public getObject(): egret.DisplayObject {
+        return this.block;
+    }
+
+    public getBlockInfo(): BlockInfo {
+        return this.info;
+    }
+
+    public getRow(): number {
+        return this.info.row;
+    }
+
+    public getCol() : number {
+        return this.info.col;
+    }
+
+    public getSpecialEffect() {
+        return this.info.effectType != undefined ? this.info.effectType : null;
+    }
+
+    public getType() {
+        return this.type;
+    }
+
+    /**
+     * 根据特定方向移动一个格子
+     * @param dir 移动方向
+     * @param callback 移动完成后回调
+     * @param waitTime 延时多长时间之后执行移动操作
+     */
+    public move(dir:number, callback?:Function, waitTime?:number) {
         let props = null;
 
         switch(dir) {
@@ -71,45 +145,6 @@ class BlockBase {
         }
     }
 
-    public draw() {
-        this.block = Util.createBitmapByName(this.getImgRes());
-    }
-
-    public drop(count: number, callback?: Function) {
-        let tw = egret.Tween.get(this.block);
-        tw.wait(100).to({
-            y: this.block.y + this.info.height*count
-        }, 200, egret.Ease.quartIn).call(() => {
-            this.info.row += count;
-            egret.Tween.removeTweens(tw);
-            if (callback) {
-                callback();
-            }
-        });
-    }
-
-    public getBgImg() {}
-
-    public getImgRes() {
-        return "bear_png";
-    }
-
-    public getObject(): egret.DisplayObject {
-        return this.block;
-    }
-
-    public getBlockInfo(): BlockInfo {
-        return this.info;
-    }
-
-    public getRow(): number {
-        return this.info.row;
-    }
-
-    public getCol() : number {
-        return this.info.col;
-    }
-
     public setSize() {
         let borderSize = this.info.width * this.borderRatio;
 
@@ -119,7 +154,30 @@ class BlockBase {
         this.block.height = this.info.height - (borderSize*2);
     }
 
-    public setType(type: BLOCK_TYPE) {
-        this.type = type;
+    public show(container: egret.DisplayObjectContainer) {
+        container.addChild(this.block);
+        if (this.info.effectType != undefined && this.block instanceof egret.MovieClip) {
+            switch (this.info.effectType) {
+                case EFFECT_TYPE.ROW_LINE: {
+                    this.block.gotoAndPlay("rowline", -1);
+                    break;
+                }
+                case EFFECT_TYPE.COL_LINE: {
+                    this.block.gotoAndPlay("colline", -1);
+                    break;
+                }
+                case EFFECT_TYPE.BOMB: {
+                    this.block.gotoAndPlay("bomb", -1);
+                    break;
+                }
+                case EFFECT_TYPE.MAGIC_BIRD: {
+                    this.block.gotoAndPlay("magicbird", -1);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
     }
 }
