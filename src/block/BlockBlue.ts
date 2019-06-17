6b062f4a6792dafc23e5d14063efbb9a77bf9243
gaonee <18551750323@163.com>
2019-06-17 20:06:36 +0800

    617

--- src/block/BlockBlue.ts
+++ src/block/BlockBlue.ts
@@ -9,13 +9,22 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 class BlockBlue extends BlockBase {
-    public imgRes: string = "chick_png";
     public readonly type: number = BLOCK_TYPE.BLUE;
     constructor(info: BlockInfo) {
         super(info);
     }
 
-    public getImgRes() {
-        return "chick_png";
+    /*
+    * 绘制普通状态
+    **/
+    public drawNormal(): egret.Bitmap {
+        return Util.createBitmapByName("chick_png");
+    }
+
+    /*
+    * 绘制特效
+    **/
+    public drawSpecialEffect(): egret.MovieClip {
+        return Util.createMCByName("mcchick");
     }
 }
