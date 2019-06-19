167f486dbadb877246d9a8c08e16c6da82874c02
gaonee <18551750323@163.com>
2019-06-19 17:41:20 +0800

    619

--- src/map/BlockMap.ts
+++ src/map/BlockMap.ts
@@ -14,9 +14,7 @@ class BlockMap {
     private row: number = 0;
     private column: number = 0;
     private size: number = 0;
-    // 标识哪一列有格子消除，并记录每列最下方的消除位置
-    private eliminateMemo: number[] = null;
-    private effectsProcess: EffectsProcess = null;
+    private effectsProcess: EliminateProcess = null;
 
     constructor(container: egret.DisplayObjectContainer, row: number, col: number, size: number) {
         if (container == null) return;
@@ -25,193 +23,12 @@ class BlockMap {
         this.row = row;
         this.column = col;
         this.size = size;
-        this.effectsProcess = new EffectsProcess();
+        this.effectsProcess = new EliminateProcess();
 
         for (let row = 0; row < this.row; row++) {
             this.map[row] = new Array();
         }
     }
-
-    /*
-     * Create special effect block.
-     * @param eliminateInfo 消除信息
-     * @param sourceBlockInfo 源信息，表示是哪种类型的格子消除产生的，
-     * 用来继承一些公共的属性。
-    **/
-    private createEffectBlock(eliminateInfo: EliminateInfo, sourceBlockInfo: BlockInfo) {
-        let effectType: EFFECT_TYPE = null;
-        let type = eliminateInfo.type;
-
-        switch (type) {
-            case ELIMINATE_TYPE.COL_LINE_THREE:
-            case ELIMINATE_TYPE.ROW_LINE_THREE: {
-                break;
-            }
-            case ELIMINATE_TYPE.ROW_LINE_FOUR: {
-                effectType = EFFECT_TYPE.COL_LINE;
-                break;
-            }
-            case ELIMINATE_TYPE.COL_LINE_FOUR: {
-                effectType = EFFECT_TYPE.ROW_LINE;
-                break;
-            }
-            case ELIMINATE_TYPE.ROW_LINE_FIVE: 
-            case ELIMINATE_TYPE.COL_LINE_FIVE: {
-                effectType = EFFECT_TYPE.MAGIC_BIRD;
-                break;
-            }
-            case ELIMINATE_TYPE.NON_LINE: {
-                effectType = EFFECT_TYPE.BOMB;
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-
-        if (effectType != null) {
-            let point = eliminateInfo.keyPoint;
-            let blockInfo = {
-                row: point.row,
-                col: point.col,
-                width: sourceBlockInfo.width,
-                height: sourceBlockInfo.height,
-                type: sourceBlockInfo.type,
-                effectType: effectType
-            }
-            let block = Util.createBlock(blockInfo);
-            block.show(this.container);
-            this.add(point.row, point.col, block);
-            Log.debug("effectsEliminate, type: " + type + "; keyPoint, row: " + point.row+",col:" + point.col);
-        }
-    }
-
-    /*
-     * 下降后位置更新
-    **/
-    private dropColList(col: number, dropList, callback?: Function): void {
-        let finishCount = 0;
-        for (let row = 0; row < dropList.length; row++) {
-            let block: BlockBase = dropList[row].block;
-            block.drop(dropList[row].count, () => {
-                finishCount ++;
-                if (finishCount == dropList.length) {
-                    for (let r = 0; r < dropList.length; r++) {
-                        this.map[r][col] = dropList[r].block;
-                    }
-                    callback && callback();
-                }
-            })
-        }
-    }
-
-    /*
-     * 填充和下落
-     * @param eliminateArr 哪些列有格子消除
-    **/
-    private dropDown(callback?: Function) {
-        let dropCount = 0;
-        for (let i = 0; i < this.column; i++) {
-            let info = this.eliminateMemo[i];
-            if (info == undefined) continue;
-
-            let count = 0;
-            let dropList = new Array();
-
-            for (let row = this.row-1; row >= 0; row--) {
-                let block = this.get(row, i);
-                if (block == null) {
-                    count ++;
-                } else {
-                    if (count > 0) {
-                        dropList.unshift({
-                            count: count,
-                            block: block
-                        });
-                    }
-                }
-            }
-
-            if (count <= 0) continue;
-
-            dropCount ++;
-
-            // 上方生成新的格子，补足消除的空缺
-            for (let c = 1; c <= count; c++) {
-                // let index = count - c + 1;
-                let info: BlockInfo = Util.generateBlockInfo(-c, i, this.size);
-                let block: BlockBase = Util.createBlock(info);
-                block.show(this.container);
-                dropList.unshift({
-                    count: count,
-                    block: block
-                });
-            }
-            this.dropColList(i, dropList, () => {
-                dropCount --;
-                if (dropCount == 0) {
-                    callback && callback();
-                }
-            });
-        }
-    }
-
-    /*
-     * 消除目标格子。
-     * @param points 需要消除的格子的位置组成的数组
-    **/
-    private eliminateBlocks(points: Point[]) {
-        for (let i = 0; i < points.length; i++) {
-            let p = points[i];
-            let block = this.get(p.row, p.col);
-            if (block == null) continue;
-
-            this.remove(p.row, p.col);
-            
-            // 是否是特效
-            let effect: EFFECT_TYPE = block.getSpecialEffect();
-            if (effect != null) {
-                this.effectEliminate(block);
-            }
-
-            this.setEliminateMemo(p.row, p.col);
-        }
-    }
-
-    /*
-     * 特效消除。
-     * @param block 需要消除的特效格子
-     * @param point 格子位置
-    **/
-    private effectEliminate(block: BlockBase) {
-        switch (block.getSpecialEffect()) {
-            case EFFECT_TYPE.ROW_LINE: {
-                this.effectsProcess.eliminateRow(this, block.getRow());
-                break;
-            }
-            case EFFECT_TYPE.COL_LINE: {
-                this.effectsProcess.eliminateCol(this, block.getCol());
-                break;
-            }
-            case EFFECT_TYPE.BOMB: {
-                this.effectsProcess.eliminateBomb(this, block.getPoint());
-                break;
-            }
-            case EFFECT_TYPE.MAGIC_BIRD: {}
-            default: {
-                break;
-            }
-        }
-    }
-
-    /*
-     * 记录每列最下方消除位置，从而得知哪些格子产生了移动，
-     * 进而，得到触发特效形成的keyPoint
-    **/
-    public setEliminateMemo(row: number, col: number) {
-        this.eliminateMemo[col] = this.eliminateMemo[col] == undefined ? row : Math.max(row, this.eliminateMemo[col]);
-    }
-
 
     /*
      * 填充格子
@@ -237,29 +54,6 @@ class BlockMap {
             return block.type == type;
         }
         return false;
-    }
-
-    /*
-     * 根据指定点进行消除
-     * @param eliminateList EliminateInfo组成的数组
-    **/
-    public eliminate(eliminateList: EliminateInfo[], callback: Function) {
-        this.eliminateMemo = new Array(this.column);
-        for (let i = 0; i < eliminateList.length; i++) {
-            let eliminateInfo = eliminateList[i];
-            if (!Array.isArray(eliminateInfo.points)) {
-                Log.warn("effectsEliminate, points is null");
-                continue;
-            }
-            // 消除之前获取，否则不存在
-            let copyInfo: BlockInfo = this.get(eliminateInfo.points[0].row, eliminateInfo.points[0].col).getBlockInfo();
-            this.eliminateBlocks(eliminateInfo.points);
-            this.createEffectBlock(eliminateInfo, copyInfo);
-        }
-
-        this.dropDown(() => {
-            setTimeout(callback, 50);
-        });
     }
 
     /*
@@ -301,8 +95,17 @@ class BlockMap {
         return this.column;
     }
 
+    public getContainer(): egret.DisplayObjectContainer {
+        return this.container;
+    }
+
+    public getSize(): number {
+        return this.size;
+    }
+
     public hasDrop(row: number, col: number): boolean {
-        return this.eliminateMemo[col] != undefined && this.eliminateMemo[col] >= row;
+        let eliminateMemo = this.effectsProcess.eliminateMemo;
+        return eliminateMemo[col] != undefined && eliminateMemo[col] >= row;
     }
 
     /*
@@ -355,7 +158,45 @@ class BlockMap {
         }
     }
 
-    /*
+    public set(row: number, col: number, block: BlockBase) {
+        this.map[row][col] = block;
+    }
+
+    /**
+     * 简单消除，只根据指定点进行消除。
+     * @param eliminateList EliminateInfo组成的数组
+     * @param callback 消除并补全、下落完成后调用
+    **/
+    public simpleEliminateProcess(eliminateList: EliminateInfo[], callback: Function) {
+        this.effectsProcess.simple(this, eliminateList, callback);
+    }
+
+    /**
+     * 双特效交换消除
+     * @param touchBlock 手指触摸的格子
+     * @param swapBlock 被交换的格子
+     * @param callback 消除并补全、下落完成后调用
+    **/
+    public effectSwapEliminateProcess(touchBlock: BlockBase, swapBlock: BlockBase, callback: Function) {
+        let touchBlockEffect = touchBlock.getSpecialEffect();
+        let swapBlockEffect = swapBlock.getSpecialEffect();
+
+        switch (touchBlockEffect) {
+            case EFFECT_TYPE.ROW_LINE: {
+                switch (swapBlockEffect) {
+                    case EFFECT_TYPE.ROW_LINE: {}
+                    case EFFECT_TYPE.COL_LINE: {}
+                    case EFFECT_TYPE.BOMB: {}
+                    case EFFECT_TYPE.MAGIC_BIRD: {}
+                }
+            }
+            case EFFECT_TYPE.COL_LINE: {}
+            case EFFECT_TYPE.BOMB: {}
+            case EFFECT_TYPE.MAGIC_BIRD: {}
+        }
+    }
+
+    /**
      * 交换相邻位置格子
     **/
     public swap(distRow: number, distCol: number, resRow: number, resCol: number): void {
